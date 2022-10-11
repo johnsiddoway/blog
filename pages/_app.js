b9f9856affe70828e62de88b075f1223073e60b4
John Siddoway <john.pez.s@gmail.com>
2022-10-10 22:25:44 -0700

    Clean up styling a little on the index and archive pages.

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,7 @@ export default function App({ Component, pageProps }) {
     return <>
         <Head>
             <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            <title>John Siddoway Blog: Maps and Gold</title>
         </Head>
         <Component {...pageProps} />
     </>;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 import Link from 'next/link';
-import Head from 'next/head';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
-import { Date } from '../components';
+import { BlogListItem } from '../components';
 import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
@@ -15,22 +12,15 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
-    return <>
-        <Head>
-            <title>John Siddoway Blog: Maps and Gold</title>
-        </Head>
+    const listItems = allPostsData.slice(0, 5).map((post) =>
+        <BlogListItem key={post.id} id={post.id} title={post.title} date={post.date}/>
+    );
+    return <article>
         <ul className="list-unstyled">
-            {allPostsData.slice(0, 5).map(({ id, date, title }) => (
-                <li key={id}>
-                    <h3 className="text-primary"><Link href={`/posts/${id}`}><a>{title}</a></Link></h3>
-                    <div className="border-dark border-top border-bottom mb-3">
-                        <div className="d-inline">
-                            <FontAwesomeIcon icon={faCalendarAlt} fixedWidth />
-                            <Date dateString={date} />
-                        </div>
-                    </div>
-                </li>
-            ))}
+            {listItems}
         </ul>
-    </>;
+        <h3 className="text-center">
+            <Link href='/archive'><a>Blog Archive</a></Link>
+        </h3>
+    </article>;
 }
