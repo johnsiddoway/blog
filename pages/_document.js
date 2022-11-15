1543b0541146a7b2659979a741644018997be17a
John Siddoway <john.pez.s@gmail.com>
2022-11-14 22:28:02 -0800

    Got a couple of posts with custom javascript and stylesheets to work again.

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,9 @@ export default function Document() {
         <Head>
             <link rel="icon" href="/favicon.png" type="image/png" />
             <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Exo+2|Fira+Code|Open+Sans|Source+Sans+Pro" />
+            {/* TODO: Move these from generic imports to only being included on pages that need them */}
+            <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossOrigin="anonymous" />
+            <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
             <meta name="description" content="John Siddoway Blog" />
             <meta name="og:title" content="John Siddoway Blog: Maps and Gold" />
         </Head>
