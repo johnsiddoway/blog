ab8f3e82c70ddfd25b6c0a201bd83da813786be4
John Siddoway <john.pez.s@gmail.com>
2022-11-19 17:32:31 -0800

    Tweak styling a little to get the footer to properly sit at the bottom. Move Calendar icon into the Date component.

--- components/bloglistitem.js
+++ components/bloglistitem.js
@@ -1,18 +1,15 @@
 import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
 import { Date } from './date';
 
 export function BlogListItem(props) {
     return <li className="d-flex border-bottom border-dark mb-2 pb-2">
-        <div className="h4 mb-0 text-decoration-none">
+        <h4 className="mb-0">
             <Link href={`/posts/${props.id}`}>
                 <a className="text-decoration-none">{props.title}</a>
             </Link>
-        </div>
+        </h4>
         <div className="d-none d-md-block ms-auto mt-auto">
             <Date dateString={props.date} />
-            <FontAwesomeIcon icon={faCalendarAlt} fixedWidth className="ms-1" />
         </div>
     </li>
 }
