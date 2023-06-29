f2b317eafaa4299f935b7d88cb65d7c3829c0d23
John Siddoway <john.pez.s@gmail.com>
2023-06-29 09:19:16 -0700

    Hopefully complete process to generate static files for site.

--- prerender.js
+++ prerender.js
@@ -12,23 +12,35 @@ const template = fs.readFileSync(toAbsolute('dist/static/index.html'), 'utf-8')
     .replace(/\s*\<link.*href\=\"\/assets\/vendor.*\.js\"\>/, '');
 const { render } = await import('./dist/server/entry-server.js');
 
-// determine routes to pre-render from src/pages
-const routesToPrerender = ['/', '/about', '/archive'];
 async function prerenderPages() {
+    // determine routes to pre-render from src/pages
+    const routesToPrerender = ['/', '/about', '/archive'];
+
     for (const url of routesToPrerender) {
         const context = {};
         const appHtml = await render(url, context);
-
         const html = template.replace(`<!--app-html-->`, appHtml);
-
         const filePath = `dist/static${url === '/' ? '/index' : url}.html`;
         fs.writeFileSync(toAbsolute(filePath), html);
-        console.log('pre-rendered:', filePath);
+        console.log('pre-rendered: ', filePath);
     }
 }
 
 async function prerenderPosts() {
-    console.info(`Attempting to prerender /src/posts`);
+    console.info(`Attempting to prerender src/posts`);
+    const postsToPrerender = fs.readdirSync(toAbsolute('src/posts'));
+    for (const filePath of postsToPrerender) {
+        const datePart = filePath.slice(0, 11).replaceAll('-', '/');
+        const namePart = filePath.slice(11, -3);
+        const url = `/posts/${datePart}${namePart}`;
+        const appHtml = await render(url, {});
+        const html = template.replace(`<!--app-html-->`, appHtml);
+        const outPath = toAbsolute(`dist/static${url}.html`);
+        const outDir = path.dirname(outPath);
+        fs.mkdirSync(outDir, { recursive: true });
+        fs.writeFileSync(outPath, html);
+        console.log(`prerendered: ${outPath}`);
+    }
 }
 
 (async () => {
