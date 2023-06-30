66beaa338f52ea82ab1c390c957f3da6a8d50d8d
John Siddoway <john.pez.s@gmail.com>
2023-06-30 09:55:14 -0700

    I think I got error handling working for both client-side and server-side rendering.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -2,9 +2,9 @@ import ReactDOMServer from 'react-dom/server'
 import { StaticRouter } from 'react-router-dom/server'
 import App from './app';
 
-export function render(url, context) {
+export function render(url: any) {
     return ReactDOMServer.renderToString(
-        <StaticRouter location={url} context={context}>
+        <StaticRouter location={url}>
             <App />
         </StaticRouter>,
     )
---
+++ src/pages/error-boundary.tsx
@@ -0,0 +1,7 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorBoundary() {
+    let error = useRouteError();
+    console.error(error);
+    return <div>Dang!</div>;
+}
