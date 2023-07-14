f13c9922b343c840d34af6345ba9aabc8172d5c9
John Siddoway <john.pez.s@gmail.com>
2023-07-14 13:28:37 -0700

    Use @johnsiddoway/sass@1.0.5. Add font awesome icons to the post headers.

--- src/components/post-list-item.tsx
+++ src/components/post-list-item.tsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import { PostDetail } from "../lib/posts";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCalendarDays } from "@fortawesome/free-regular-svg-icons";
 
 export interface PostListItemProps {
 	key: string;
@@ -8,6 +10,9 @@ export interface PostListItemProps {
 export default function PostListItem({ post }: PostListItemProps) {
 	return <Link to={post.url} className="post-list-item">
 		<h2>{post.title}</h2>
-		<div className="date">{post.date}</div>
+		<div className="date-with-icon">
+			{post.date}
+			<FontAwesomeIcon icon={faCalendarDays} />
+		</div>
 	</Link>;
 }
