b7ea99d0e7d0d612faa0c3d2e141cf11d03869fb
John Siddoway <john.pez.s@gmail.com>
2023-09-03 15:18:02 -0700

    Uncomment out the section to generate static pages.

--- prerender.js
+++ prerender.js
@@ -13,19 +13,19 @@ const template = fs.readFileSync(toAbsolute('dist/static/index.html'), 'utf-8')
 	.replace(/\s*\<link.*href\=\"\/assets\/vendor.*\.js\"\>/, '');
 const { render } = await import('./dist/server/entry-server.js');
 
-// async function prerenderPages() {
-// 	// determine routes to pre-render from src/pages
-// 	const routesToPrerender = ['/', '/about', '/archive', '/404'];
+async function prerenderPages() {
+	// determine routes to pre-render from src/pages
+	const routesToPrerender = ['/', '/about', '/archive', '/404'];
 
-// 	for (const url of routesToPrerender) {
-// 		const context = {};
-// 		const appHtml = await render(url, context);
-// 		const html = template.replace(`<!--app-html-->`, appHtml);
-// 		const filePath = `dist/static${url === '/' ? '/index' : url}.html`;
-// 		fs.writeFileSync(toAbsolute(filePath), html);
-// 		console.log('pre-rendered: ', filePath);
-// 	}
-// }
+	for (const url of routesToPrerender) {
+		const context = {};
+		const appHtml = await render(url, context);
+		const html = template.replace(`<!--app-html-->`, appHtml);
+		const filePath = `dist/static${url === '/' ? '/index' : url}.html`;
+		fs.writeFileSync(toAbsolute(filePath), html);
+		console.log('pre-rendered: ', filePath);
+	}
+}
 
 async function prerenderPosts() {
 	console.info(`Attempting to prerender src/posts`);
@@ -81,6 +81,6 @@ function getUrl(name) {
 }
 
 (async () => {
-	// prerenderPages();
 	prerenderPosts();
+	prerenderPages();
 })();
