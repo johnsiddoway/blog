aa4b449bab56c29fd457d4898a327ba00d8e03a5
John Siddoway <john.pez.s@gmail.com>
2024-01-16 09:48:08 -0800

    Minor tweaks to try cleaning up partial hydration example.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,21 +2,20 @@ import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
-import polyfill from 'rollup-plugin-node-polyfills';
+import terser from '@rollup/plugin-terser';
 
 export default {
 	input: "src/components/index.js",
 	output: {
 		file: "dist/components/test.js",
 		format: "iife",
-		sourcemap: true,
+		sourcemap: false,
 	},
 	plugins: [
 		nodeResolve({
 			browser: true,
 			extensions: [".js"],
 		}),
-		nodePolyfills(),
 		replace({
 			preventAssignment: true,
 			'process.env.NODE_ENV': JSON.stringify('production')
@@ -26,5 +25,6 @@ export default {
 			babelHelpers: 'bundled',
 		}),
 		commonjs(),
+		terser(),
 	]
 };
