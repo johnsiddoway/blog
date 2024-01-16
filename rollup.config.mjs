840ad6a64c90685d0e0f2335d1da0ec30b6e9bcf
John Siddoway <john.pez.s@gmail.com>
2024-01-15 20:59:21 -0800

    Try making a couple of changes to rollup.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
+import polyfill from 'rollup-plugin-node-polyfills';
 
 export default {
 	input: "src/components/index.js",
@@ -12,8 +13,10 @@ export default {
 	},
 	plugins: [
 		nodeResolve({
+			browser: true,
 			extensions: [".js"],
 		}),
+		nodePolyfills(),
 		replace({
 			preventAssignment: true,
 			'process.env.NODE_ENV': JSON.stringify('production')
