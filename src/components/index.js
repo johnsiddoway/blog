2c64157218c57e5ef05dc7d08a57bd56c65b7365
John Siddoway <john.pez.s@gmail.com>
2024-01-17 11:37:37 -0800

    Add script to clean workspace. Remove unused index.js file.

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Component from './TimesWeMispronouncedVite.jsx';
-
-ReactDOM.createRoot(document.getElementById('react-root')).render(<Component />);
