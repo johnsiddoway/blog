5c3d2b353b1de12f55e70194593b915778d73503
John Siddoway <john.pez.s@gmail.com>
2024-01-19 13:38:33 -0800

    Clean up the color post

--- src/posts/2024-01-15-colors/index.jsx
+++ src/posts/2024-01-15-colors/index.jsx
@@ -6,7 +6,7 @@ import './index.scss';
 import "react-color-palette/css";
 
 function Component() {
-	const [color, setColor] = useColor("hex", "#F16B0E"); // #F10E23
+	const [color, setColor] = useColor("#F16B0E"); // #F10E23
 
 	return <>
 		<ColorPicker width={500} height={400} color={color} onChange={setColor} />
