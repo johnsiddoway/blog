b8e467ea0e71d1d69c343fda150e3f224bbe0131
John Siddoway <john.pez.s@gmail.com>
2024-01-18 21:20:17 -0800

    Working version of per-post jsx bundle.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,29 +5,34 @@ import replace from '@rollup/plugin-replace';
 import terser from '@rollup/plugin-terser';
 
 const plugins = [
-	nodeResolve({
-		browser: true,
-		extensions: [".js"],
-	}),
 	replace({
 		preventAssignment: true,
 		'process.env.NODE_ENV': JSON.stringify('production')
 	}),
-	babel({
-		presets: ["@babel/preset-react"],
-		babelHelpers: 'bundled',
+	nodeResolve({
+		browser: true,
+		extensions: [".js", ".jsx", ".ts", ".tsx"],
+		dedupe: ['react', 'react-dom'],
 	}),
 	commonjs(),
+	babel({
+		exclude: "**/node_modules/**",
+		presets: ["@babel/preset-env", "@babel/preset-react"],
+		babelHelpers: 'bundled',
+		extensions: [".js", ".jsx", ".ts", ".tsx"],
+	}),
 	terser(),
 ];
 
 export default [
 	{
-		input: "src/posts/2024-01-15-colors/index.js",
+		input: "src/posts/2024-01-15-colors/index.jsx",
 		output: {
 			file: "dist/posts/2024-01-15-colors/index.js",
 			format: "iife",
 			sourcemap: false,
+			name: 'page',
+			compact: true,
 		},
 		plugins: plugins,
 	}
