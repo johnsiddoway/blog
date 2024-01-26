70d6b8cd1fc46b72a81e13f4cfab9a7bc1974cd6
John Siddoway <john.pez.s@gmail.com>
2024-01-25 17:17:01 -0800

    Create robots.txt and sitemap.xml files.

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const dayjs = require('dayjs');
+const utc = require('dayjs/plugin/utc');
 const markdownItAttrs = require("markdown-it-attrs");
 const jsxPlugin = require('./jsxPlugin.js');
 const sassPlugin = require('./sassPlugin.js');
 const syntaxHighlight = require("markdown-it-highlightjs");
+
+dayjs.extend(utc);
 
 module.exports = function (eleventyConfig) {
 	// Get the first `n` elements of a collection.
@@ -15,6 +19,19 @@ module.exports = function (eleventyConfig) {
 
 		return array.slice(0, n);
 	});
+
+	eleventyConfig.addFilter("readableDate", (dateObj) => {
+		return dayjs.utc(dateObj).format("dd LLL yyyy");
+	});
+	
+	// https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+		return dayjs.utc(dateObj).format('YYYY-MM-DD');
+	});
+
+	// Put robots.txt in root
+	eleventyConfig.addPassthroughCopy({ 'src/robots.txt': '/robots.txt' });
+	eleventyConfig.addPassthroughCopy({ 'src/staticwebapp.config.json': '/staticwebapp.config.json' });
 
 	// Copy anything in the src/assets folder over to ${outputDir}/assets
 	eleventyConfig.addPassthroughCopy({ "src/assets": "/assets" });
