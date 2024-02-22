570670e3247e4c2a3fe601927250def0bfd182fb
John Siddoway <john.pez.s@gmail.com>
2024-02-21 20:59:59 -0800

    Remove unused components directory.

--- src/components/TimesWeMispronouncedVite.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function TimesWeMispronouncedVite() {
-	const [count, setCount] = React.useState(0);
-	return <div>
-		<p>I've said Vite wrong {count} times today</p>
-		<button className="btn btn-primary" onClick={() => setCount(count + 1)}>Add one</button>
-	</div>;
-};
