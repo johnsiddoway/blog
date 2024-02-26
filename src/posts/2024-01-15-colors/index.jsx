0173998c223f46bdf7f4b095b3cc9eb536ed315c
John Siddoway <john.pez.s@gmail.com>
2024-02-25 20:27:20 -0800

    Working on getting a dynamic number of theme colors working.

--- src/posts/2024-01-15-colors/index.jsx
+++ src/posts/2024-01-15-colors/index.jsx
@@ -5,8 +5,6 @@ import './index.scss';
 import { useClickOutside } from './_useClickOutside';
 import { SwatchPanel } from './_swatch-panel';
 
-/* background: url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg==") left center fixed */
-
 function PopoverPicker({ color, onChange }) {
 	const popover = useRef();
 	const [isOpen, toggle] = useState(false);
@@ -33,31 +31,82 @@ function ColorPicker({ color, onChange, label }) {
 	return <div className="colorPicker">
 		<label for={label}>{label}</label>
 		<div className="inputGroup">
-			<input id={label} value={color} />
+			<input id={label} value={color} onChange={(e) => onChange(e.target.value)} />
 			<PopoverPicker color={color} onChange={onChange} />
 		</div>
 	</div>;
 }
 
+type Action =
+	| { type: 'set-foreground-color', color: string }
+	| { type: 'set-background-color', color: string }
+	| { type: 'set-theme-color', index: number, color: string }
+	| { type: 'set-theme-color-count', count: number };
+type State = {
+	foregroundColor: string;
+	backgroundColor: string;
+	themeColorCount: number;
+	themeColors: string[];
+}
+function reducer(state, action) {
+	switch (action.type) {
+		case 'set-foreground-color':
+			return {
+				...state,
+				foregroundColor: action.color,
+			};
+		case 'set-background-color':
+			return {
+				...state,
+				backgroundColor: action.color,
+			};
+		case 'set-theme-color':
+			return {
+				...state,
+				themeColors: state.themeColors.map((color, index) => {
+					if (index == action.index) {
+						return action.color;
+					} else {
+						return color;
+					}
+				}),
+			};
+		case 'set-theme-color-count':
+			return {
+				...state,
+				themeColorCount: action.count,
+			};
+		default:
+			return state;
+	}
+};
+
 function Component() {
-	const [foregroundColor, setForegroundColor] = useState("#000");
-	const [backgroundColor, setBackgroundColor] = useState("#fff");
-	const [primary, setPrimary] = useState('#fff');
-	const [secondary, setSecondary] = useState('#777');
-	const [tertiary, setTertiary] = useState('#000');
+	const [{ foregroundColor, backgroundColor, themeColorCount, themeColors }, dispatch] = useReducer(reducer, { foregroundColor: '#292521', backgroundColor: '#fff', themeColorCount: 1, themeColors: ['#daa520'] });
 
 	document.body.style.backgroundColor = backgroundColor;
 
+	const themeColorPickers = [];
+	for (i = 0; i < number; i++) {
+		themeColorPickers.push(<ColorPicker color={themeColors[i]} onChange={(e) => dispatch({ type: 'set-theme-color', index: i, color: e })} />);
+	}
+
 	return <>
 		<div className="pickerPanel">
-			<ColorPicker color={foregroundColor} onChange={setForegroundColor} label="Text" />
-			<ColorPicker color={backgroundColor} onChange={setBackgroundColor} label="Background" />
-			<ColorPicker color={primary} onChange={setPrimary} label="Primary" />
-			<ColorPicker color={secondary} onChange={setSecondary} label="Secondary" />
-			<ColorPicker color={tertiary} onChange={setTertiary} label="Tertiary" />
+			<ColorPicker color={foregroundColor} onChange={(e) => dispatch({ type: 'set-foreground-color', color: e })} label="Text" />
+			<ColorPicker color={backgroundColor} onChange={(e) => dispatch({ type: 'set-background-color', color: e })} label="Background" />
+			<select name="count" id="count" onChange={(e) => dispatch({ type: 'set-theme-color-count', count: e.target.value })}>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+			</select>
+			{themeColorPickers}
 		</div>
 		<SwatchPanel color={primary} />
 	</>;
 }
 
 createRoot(document.getElementById('react-root')).render(<Component />);
+
+// document.body.style.background = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg==") left center fixed';
