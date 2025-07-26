fdf4186c3d95b3e36b190394bf28551aff0fdef0
John Siddoway <john.pez.s@gmail.com>
2025-07-26 12:09:21 -0700

    Initial commit to reset the repo following the Eleventy Getting Started guide: https://www.11ty.dev/docs/ .

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+export default async function(eleventyConfig) {
+	// Configure Eleventy
+
+    return {
+		dir: {
+			input: "src",
+			output: "dist"
+		}
+	}
+};
--- src/posts/2024/05/15/style-test/index.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('[data-dialog]').forEach((link) => {
-    link.addEventListener("click", (event) => {
-        event.preventDefault();
-        const dialog = document.querySelector('dialog#' + event.target.getAttribute('data-dialog'));
-        dialog.showModal();
-    });
-});
