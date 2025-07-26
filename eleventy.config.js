192636b36a36580ade729be3e04ae7551d459ec8
John Siddoway <john.pez.s@gmail.com>
2025-07-26 13:19:11 -0700

    Add eslint and fix errors.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,10 +1,11 @@
-export default async function(eleventyConfig) {
-	// Configure Eleventy
+// eslint-disable-next-line  @typescript-eslint/no-unused-vars
+export default async function (eleventyConfig) {
+    // Configure Eleventy
 
     return {
-		dir: {
-			input: "src",
-			output: "dist"
-		}
-	}
+        dir: {
+            input: "src",
+            output: "dist",
+        },
+    };
 };
---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+// @ts-check
+
+import eslint from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+import tseslint from "typescript-eslint";
+
+// https://typescript-eslint.io/getting-started
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    // https://eslint.style/guide/config-presets
+    // https://github.com/eslint-stylistic/eslint-stylistic/blob/main/packages/eslint-plugin/configs/customize.ts
+    stylistic.configs.customize({
+        indent: 4,
+        quotes: "double",
+        semi: true,
+    }),
+);
